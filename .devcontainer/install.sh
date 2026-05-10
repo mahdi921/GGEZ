1ac2cf527d4221e9ab6026fed079816ad2fa3f17
Mahdi Afshar <mahdi921@users.noreply.github.com>
2026-05-10 10:33:58 +0330

    Update install.sh
    Use curl instead of wget
    Changed the name of zipped file to its original

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "downloading xray"
-wget -O ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
+curl -fsSL ${PWD}/xray.zip https://github.com/XTLS/Xray-core/releases/download/v26.3.27/Xray-linux-64.zip
 
 echo "installing"
-unzip xray.zip && chmod +x xray
+unzip xray-linux-64.zip && chmod +x xray
 mv xray /usr/local/bin/xray
 
 rm -rf ${PWD}/*
